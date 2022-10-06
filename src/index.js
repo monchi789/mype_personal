b2130df50b19900495ab6cf242f6ecb339772286
Cristian <Cristian-02-@live.com>
2022-10-06 16:38:57 -0500

    Add the changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.get('/', (req, res) => res.send('Mype Personal'))
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+app.use(express.static(join(__dirname, 'public')))
 
 app.listen(3002)
 console.log('Server listening on port ', 3002)
